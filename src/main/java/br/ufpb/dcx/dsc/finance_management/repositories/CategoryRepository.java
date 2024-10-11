5ad2229bbed841e77a94d8cd141febdf5a18e9dc
ivanildo <ivanildojunior2319@gmail.com>
2024-10-11 17:58:28 -0300

    DTO, Controller, repository, services de category criado

--- src/main/java/br/ufpb/dcx/dsc/finance_management/repositories/CategoryRepository.java
+++ src/main/java/br/ufpb/dcx/dsc/finance_management/repositories/CategoryRepository.java
@@ -1,4 +1,9 @@
 package br.ufpb.dcx.dsc.finance_management.repositories;
 
-public class CategoryRepository {
+import br.ufpb.dcx.dsc.finance_management.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByUserId(Long userId);
 }
